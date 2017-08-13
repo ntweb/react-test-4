f71d7dd8d0b152d43366cb5030877044fd16fdd5
ntweb <nathanxp@hotmail.com>
2017-08-13 18:55:25 +0200

    geriing List

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,8 +12,6 @@ export function fetchPosts() {
     const url = `${API_URL}/posts${API_KEY}`;
     const request = axios.get(url);
 
-    console.log(request);
-
     return {
         type: FETCH_POSTS,
         payload: request
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts} from "../actions";
@@ -8,13 +9,23 @@ class PostIndex extends Component {
         this.props.fetchPosts();
     }
 
-    render() {
+    renderPosts() {
 
-        //console.log(this.prop.posts);
+        return _.map(this.props.posts, post => {
+            return (
+                <li key={post.id} className="list-group-item">Title: {post.title}</li>
+            );
+        });
+    }
+
+    render() {
 
         return (
             <div>
-                Post Index
+                <h3>Post</h3>
+                <ul className="list-group">
+                    { this.renderPosts() }
+                </ul>
             </div>
         );
     }
